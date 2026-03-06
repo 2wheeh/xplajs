458a65d0addb2a311e965e669ef7a13b56df35f4
Wonhee Lee <2wheeh@gmail.com>
2026-03-06 17:18:33 +0900

    refactor: unify e2e vitest config

--- e2e/starship/globalSetup.ts
+++ e2e/starship/globalSetup.ts
@@ -2,14 +2,10 @@ import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { ConfigContext, useRegistry } from 'starshipjs';
 
-import { it, beforeAll } from 'vitest';
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-beforeAll(async () => {
-  const configFile = join(__dirname, '..', 'configs', 'config.yaml');
+export async function setup() {
+  const configFile = join(__dirname, 'configs', 'config.yaml');
   ConfigContext.setConfigFile(configFile);
   ConfigContext.setRegistry(await useRegistry(configFile));
-});
-
-it('should ', () => {});
+}
